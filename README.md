79729bf0cc12a4f0dda592fde04c588969dafd3c
liron-li <liron.li@outlook.com>
2018-03-20 15:14:35 +0800

    完善注释

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # php-zookeeper-ide-helper
 php-zookeeper extension ide helper
+
+# Usage
+### composer
+```
+composer require --dev liron-li/php-zookeeper-ide-helper
+```
--- src/Zookeeper.php
+++ src/Zookeeper.php
@@ -2,16 +2,41 @@
 
 /**
  * Zookeeper class.
+ * reference: https://secure.php.net/manual/en/class.zookeeper.php
  */
 class Zookeeper
 {
-    /* class constants */
+    /**
+     * Can read nodes value and list its children
+     */
     const PERM_READ = 1;
+
+    /**
+     * Can set the nodes value
+     */
     const PERM_WRITE = 2;
+
+    /**
+     * Can create children
+     */
     const PERM_CREATE = 4;
+
+    /**
+     * Can delete children
+     */
     const PERM_DELETE = 8;
+
+    /**
+     * Can execute set_acl()
+     */
     const PERM_ADMIN = 16;
+
+    /**
+     * All of the above flags ORd together
+     */
     const PERM_ALL = 31;
+
+
     const EPHEMERAL = 1;
     const SEQUENCE = 2;
     const EXPIRED_SESSION_STATE = -112;
@@ -60,79 +85,167 @@ class Zookeeper
     {
     }
 
+    /**
+     * @param string $host
+     * @param callable $watcher_cb
+     * @param int $recv_timeout
+     * @return void
+     */
     public function connect($host, $watcher_cb = null, $recv_timeout = 10000)
     {
     }
 
+    /**
+     * @param string $path
+     * @param string $value
+     * @param array $acl
+     * @param int $flags
+     * @return string
+     */
     public function create($path, $value, $acl, $flags = null)
     {
     }
 
+    /**
+     * @param string $path
+     * @param int $version
+     * @return bool
+     */
     public function delete($path, $version = -1)
     {
     }
 
+    /**
+     * @param string $path
+     * @param string $data
+     * @param int $version
+     * @param null $stat
+     * @return string
+     */
     public function set($path, $data, $version = -1, &$stat = null)
     {
     }
 
+    /**
+     * @param string $path
+     * @param callable $watcher_cb
+     * @param array $stat
+     * @param int $max_size
+     * @return string
+     */
     public function get($path, $watcher_cb = null, &$stat = null, $max_size = 0)
     {
     }
 
+    /**
+     * @param string $path
+     * @param callable $watcher_cb
+     * @return array
+     */
     public function getChildren($path, $watcher_cb = null)
     {
     }
 
+    /**
+     * @param string $path
+     * @param callable $watcher_cb
+     * @return bool
+     */
     public function exists($path, $watcher_cb = null)
     {
     }
 
+    /**
+     * @param string $path
+     * @return array
+     */
     public function getAcl($path)
     {
     }
 
+    /**
+     * @param string $path
+     * @param int $version
+     * @param array $acls
+     * @return bool
+     */
     public function setAcl($path, $version, $acls)
     {
     }
 
+    /**
+     * @return int
+     */
     public function getClientId()
     {
     }
 
+    /**
+     * @param callable $watcher_cb
+     * @return bool
+     */
     public function setWatcher($watcher_cb)
     {
     }
 
+    /**
+     * @return int
+     */
     public function getState()
     {
     }
 
+    /**
+     * @return int
+     */
     public function getRecvTimeout()
     {
     }
 
+    /**
+     * @param string $scheme
+     * @param string $cert
+     * @param callable $completion_cb
+     * @return bool
+     */
     public function addAuth($scheme, $cert, $completion_cb = null)
     {
     }
 
+    /**
+     * @return bool
+     */
     public function isRecoverable()
     {
     }
 
+    /**
+     * @param resource $file
+     * @return bool
+     */
     public function setLogStream($file)
     {
     } // TODO: might be able to set a stream like php://stderr or something
 
+    /**
+     *
+     */
     public function getResultMessage()
     {
     }
 
-    // static methods
+    /**
+     * @param int $level
+     * @return bool
+     */
     static public function setDebugLevel($level)
     {
     }
 
+    /**
+     * @param bool $trueOrFalse
+     * @return bool
+     */
     static public function setDeterministicConnOrder($trueOrFalse)
     {
     }
